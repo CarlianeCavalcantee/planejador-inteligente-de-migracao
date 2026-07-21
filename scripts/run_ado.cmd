e36d60835705cd8d82b0de4e5d396c580e85b106
Carliane Cavalcante <carliane.cavalcante@bscash.com.br>
2026-07-21 17:03:57 -0300

    chore: ignore run_ado.cmd to prevent token exposure

--- scripts/run_ado.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-set ADO_ORG=https://dev.azure.com/bscash
-set ADO_PROJECT=Novos Produtos e Melhorias
-set ADO_PAT=<seu_ado_pat>
-set ADO_EPIC_ID=13049
-
-python reports/azuredevops_export.py --create-skip
